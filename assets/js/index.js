b86a4b093810beedde374481a785b1e486cd244c
xuyan <591092697@qq.com>
2021-02-24 09:18:27 +0800

    完成了主页功能

--- assets/js/index.js
+++ assets/js/index.js
@@ -18,7 +18,19 @@ function getUserInfo() {
             }
             // 调用 renderAvatar 渲染用户的头像
             renderAvatar(res.data)
-        }
+        },
+        // 不论成功还是失败，最终都会调用 complete 回调函数  也可放在baseApi.js中
+        // complete: function (res) {
+        //     // console.log('执行了 complete 回调：')
+        //     // console.log(res)
+        //     // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+        //     if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+        //         // 1. 强制清空 token
+        //         localStorage.removeItem('token')
+        //         // 2. 强制跳转到登录页面
+        //         location.href = '/login.html'
+        //     }
+        // }
     })
 }
 
@@ -44,3 +56,19 @@ function renderAvatar(user) {
             .show()
     }
 }
+
+const layer = layui.layer
+// 点击按钮，实现退出功能
+$('#btnLogout').on('click', function () {
+    // 提示用户是否确认退出
+    layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
+        //do something
+        // 1. 清空本地存储中的 token
+        localStorage.removeItem('token')
+        // 2. 重新跳转到登录页面
+        location.href = '/login.html'
+
+        // 关闭 confirm 询问框
+        layer.close(index)
+    })
+})
